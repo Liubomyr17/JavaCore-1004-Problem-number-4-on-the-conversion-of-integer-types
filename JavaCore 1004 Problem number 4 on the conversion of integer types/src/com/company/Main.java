15ac44c795ab3717fce52becd26452b4a0c9d51e
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-02-25 10:39:08 +0200

    Add files via upload

---
+++ JavaCore 1004 Problem number 4 on the conversion of integer types/src/com/company/Main.java
@@ -0,0 +1,53 @@
+package com.company;
+
+/*
+
+1004 Problem number 4 on the conversion of integer types
+Add one type conversion operation to get the answer: nine = 9
+short number = 9;
+char zero = ‘0’;
+int nine = (zero + number);
+
+Requirements:
+1. The program should display the text on the screen.
+2. You cannot change the display command. You can add only cast operators.
+3. The main () method must contain a number variable of type short.
+4. The main () method must contain a variable of type char.
+5. The main () method must contain a nine variable of type int.
+6. The initial value of the variables during initialization cannot be changed. You can add only cast operators.
+7. The program should print the number 9.
+
+ */
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystemException;
+import java.nio.file.attribute.AclFileAttributeView;
+import java.rmi.RemoteException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        float f = (float) 128.50;
+        int i = (int) f;
+        int b = (byte)(int) (i + f);
+        System.out.println(b);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
